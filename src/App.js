519c37eabb04e958db352942b1156e6e2dcfd3c1
Robin Pahlman <robin.pahlman@gmail.com>
2018-06-06 23:49:22 -0400

    set up state for App component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import './App.css';
 import Note from './Note/Note';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [];
+    }
+  }
   render() {
     return (
       <div className="App">
         <h1>React & Firebase Noteworthy App</h1>
-        <Note />
+        <Note 
+          noteContent={} 
+          noteId={} 
+          key={}
+        />
       </div>
     );
   }
