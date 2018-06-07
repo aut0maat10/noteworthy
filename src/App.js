2b4c4aa42b2ce4dd1e614094333eaef3933ae7ff
Robin Pahlman <robin.pahlman@gmail.com>
2018-06-06 23:53:48 -0400

    wrap note header and note body in divs

--- src/App.js
+++ src/App.js
@@ -7,18 +7,27 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: [];
+      notes: [
+        { id: 1, noteContent: "Note 1 here!" },
+        { id: 2, noteContent: "Note 2 here!" }
+      ];
     }
   }
   render() {
     return (
-      <div className="App">
-        <h1>React & Firebase Noteworthy App</h1>
-        <Note 
-          noteContent={} 
-          noteId={} 
-          key={}
-        />
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">
+            <h1>React & Firebase Noteworthy App</h1>
+          </div>
+          <div className="notesBody">
+            <Note 
+              noteContent={} 
+              noteId={} 
+              key={}
+            />
+          </div>
+        </div>
       </div>
     );
   }
